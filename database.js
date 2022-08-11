10d577105a5b5baab864cda3587c59e36092e8ed
Eduardo Alexandre <luis.edu.alex@gmail.com>
2022-08-11 18:42:30 -0300

    Adiciona livros a base de dados

---
+++ database.js
@@ -0,0 +1,184 @@
+const livros = [
+    {
+        id: 1,
+        nome: 'o design do dia a dia',
+        autor: 'donald a. norman',
+        categoria: 'design',
+        paginas: 271,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 2,
+        nome: 'arquitetura limpa',
+        autor: 'robert c. martin',
+        categoria: 'desenvolvimento',
+        paginas: 405,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 3,
+        nome: 'codigo limpo',
+        autor: 'robert c. martin',
+        categoria: 'desenvolvimento',
+        paginas: 424,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 4,
+        nome: 'o codificador limpo',
+        autor: 'robert c. martin',
+        categoria: 'desenvolvimento',
+        paginas: 210,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 5,
+        nome: 'shell script',
+        autor: 'aurelio marinho jargas',
+        categoria: 'desenvolvimento',
+        paginas: 480,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 6,
+        nome: 'value proposition design',
+        autor: 'alex osterwalder',
+        categoria: 'design',
+        paginas: 291,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 7,
+        nome: 'nudge',
+        autor: 'richard h. thaler',
+        categoria: 'design',
+        paginas: 325,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 8,
+        nome: 'o poder do habito',
+        autor: 'charles duhigg',
+        categoria: 'softskills',
+        paginas: 400,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 9,
+        nome: 'nao me faca pensar',
+        autor: 'steve krug',
+        categoria: 'design',
+        paginas: 199,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 10,
+        nome: 'usabilidade na web',
+        autor: 'jakob nielsen',
+        categoria: 'design',
+        paginas: 406,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 11,
+        nome: 'javaScript - o guia definitivo',
+        autor: 'david flanagan',
+        categoria: 'desenvolvimento',
+        paginas: 1062,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 12,
+        nome: 'business model generation',
+        autor: 'alexander osterwalder',
+        categoria: 'design',
+        paginas: 282,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 13,
+        nome: 'rapido e devagar',
+        autor: 'daniel kahneman',
+        categoria: 'design',
+        paginas: 607,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 14,
+        nome: 'fundamentos de html5 e css3',
+        autor: 'mauricio samy silva',
+        categoria: 'desenvolvimento',
+        paginas: 303,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 15,
+        nome: 'css grid layout',
+        autor: 'mauricio samy silva',
+        categoria: 'desenvolvimento',
+        paginas: 169,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 16,
+        nome: 'hooked',
+        autor: 'nir eyal',
+        categoria: 'design',
+        paginas: 215,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 17,
+        nome: 'sprint',
+        autor: 'jake knapp',
+        categoria: 'design',
+        paginas: 318,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 18,
+        nome: 'aprendendo a aprender',
+        autor: 'barbara oakley',
+        categoria: 'softskills',
+        paginas: 325,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 19,
+        nome: 'a Triade do tempo',
+        autor: 'christian barbosa',
+        categoria: 'softskills',
+        paginas: 265,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 20,
+        nome: 'react - aprenda praticando',
+        autor: 'mauricio samy silva',
+        categoria: 'desenvolvimento',
+        paginas: 238,
+        recomenda: true,
+        leu: true
+    }
+]
+
+module.exports = livros;
